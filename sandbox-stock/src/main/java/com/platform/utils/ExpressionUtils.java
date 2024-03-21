c9a1bf19be1b9777605b1774c97c2edd6e2d9568
xieyue86 <xieyue86@163.com>
2024-03-21 14:52:48 +0800

    springboot map 操作以及 ssh 操作

---
+++ sandbox-stock/src/main/java/com/platform/utils/ExpressionUtils.java
@@ -0,0 +1,66 @@
+package com.platform.utils;
+
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description
+ * @Author kaiyang
+ * @Date 2024-03-21 10:50 AM
+ */
+
+@Slf4j
+public class ExpressionUtils {
+
+
+    public static void main(String[] args) {
+
+        String expression = "";
+
+        // Expression compiledExp = AviatorEvaluator.compile(expression);
+
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("123", "123");
+        map1.put("234", "234");
+        map1.put("345", "345");
+        map1.put("456", "456");
+        map1.put("567", "567");
+        map1.put("678", "678");
+
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("234", "234");
+        map2.put("345", "345");
+        map2.put("456", "4568");
+        map2.put("567", "5679");
+        map2.put("678", "678");
+        map2.put("789", "789");
+
+        // map difference 的不同内容
+        MapDifference<String, String> diff = Maps.difference(map1, map2);
+        // 是否相等
+        boolean result = diff.areEqual();
+        // 左侧多的情况
+        Map<String, String> left = diff.entriesOnlyOnLeft();
+        // 右侧多的情况
+        Map<String, String> right = diff.entriesOnlyOnRight();
+        // 相同且对平的情况
+        Map<String, String> common = diff.entriesInCommon();
+
+        log.info("result is {}", result);
+        log.info("result left is {}", left);
+        log.info("result right {}", right);
+        log.info("result common {}", common);
+        // key 相同但是 value 不同的情况
+        Map<String, MapDifference.ValueDifference<String>> difference = diff.entriesDiffering();
+        MapDifference.ValueDifference<String> def = difference.get("456");
+        log.info("result difference {} left {} right {} ", difference, def.leftValue(), def.rightValue());
+
+
+    }
+
+
+}
--- sandbox-stock/src/main/java/com/platform/utils/SshUtils.java
+++ sandbox-stock/src/main/java/com/platform/utils/SshUtils.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -28,6 +29,10 @@ public class SshUtils {
     private static String password = "";
 
     public static void main(String[] args) {
+
+
+        BigDecimal amt = new BigDecimal(3.0d);
+        BigDecimal bigDecimal = BigDecimal.valueOf(3.0d);
 
         List<String> stdout = Lists.newArrayList();
 
