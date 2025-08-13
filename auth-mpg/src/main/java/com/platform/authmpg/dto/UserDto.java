d075f009ed790646f2db3b6994f5349ff5d06219
xieyue86 <xieyue86@163.com>
2025-08-13 14:51:21 +0800

    app 添加过滤器

--- auth-mpg/src/main/java/com/platform/authmpg/dto/UserDto.java
+++ auth-mpg/src/main/java/com/platform/authmpg/dto/UserDto.java
@@ -1,8 +1,11 @@
 package com.platform.authmpg.dto;
 
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.platform.authmpg.config.AppJsonSerializer;
 import lombok.Data;
+import org.apache.commons.lang3.Validate;
+import org.springframework.validation.ValidationUtils;
 import sun.misc.Contended;
 
 import java.io.Serializable;
@@ -27,5 +30,15 @@ public class UserDto implements Serializable {
 
     @JsonSerialize(using = AppJsonSerializer.class)
     private Date createTime;
+
+
+    public static void main(String[] args) {
+
+        String data = "123";
+
+        // ValidationUtils. 参数校验
+        Validate.notBlank(data, "不能为空");
+
+    }
 
 }
