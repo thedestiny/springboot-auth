3e5bff27c3eb89579eb29f4f98e7e52b632f8385
xieyue86 <xieyue86@163.com>
2025-12-03 22:28:14 +0800

    flink 笔记

--- data-cleaning/src/main/java/com/platform/migrate/flink/FlinkTest001.java
+++ data-cleaning/src/main/java/com/platform/migrate/flink/FlinkTest001.java
@@ -3,9 +3,11 @@ package com.platform.migrate.flink;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.IterativeStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
@@ -25,14 +27,39 @@ public class FlinkTest001 {
 
     public static void main(String[] args) throws Exception {
 
+        // ParameterUtil
+
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // env.getConfig().setGlobalJobParameters();
         // 间隔 20秒进行存储
         CheckpointConfig config = env.getCheckpointConfig();
         config.setCheckpointInterval(20_000);
         config.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         // 开启 checkpointing 每秒触发一次检查点
         env.enableCheckpointing(1_000);
+
+        // 读取数据
+        String words = "34 45 23";
+        // 按照单次分组并统计
+        env.fromElements(words)
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                        String[] arr = s.toLowerCase().split(" ");
+                        for (String s1 : arr) {
+                            if (s1.length() > 0) {
+                                collector.collect(new Tuple2<>(s1, 1));
+                            }
+                        }
+                    }
+                }).keyBy(0)
+                .reduce((ReduceFunction<Tuple2<String, Integer>>) (val1, val2) -> new Tuple2<>(val1.f0, val1.f1 + val2.f1))
+                .print();
+        // 分组并reduce
+
+        DataStreamSource<String> stream = env.socketTextStream("hostname", 2205);
+        // stream.flatMap().
 
 
         DataStream<Long> streamSources = env.generateSequence(0, 10000);
@@ -45,7 +72,7 @@ public class FlinkTest001 {
             }
         });
         // 进行格式转换
-        SingleOutputStreamOperator<String> map1 = iterate.map(node -> String.valueOf(2*node));
+        SingleOutputStreamOperator<String> map1 = iterate.map(node -> String.valueOf(2 * node));
         // 进行格式转换
         env.socketTextStream("localhost", 9999)
                 .flatMap(new Spliterator())
@@ -69,8 +96,6 @@ public class FlinkTest001 {
             }
         }
     }
-
-
 
 
 }
--- doc/kafka_flink_doc.md
+++ doc/kafka_flink_doc.md
@@ -44,6 +44,45 @@ sql
 tableApi
 stream table 
 
+dataStream dataSet  流批一体
+watermark 延迟时间 事件时间
+
+window 机制
+
+flink sql
+
+exactly-once 
+chandy-lamport 算法
+jobManager 触发 checkpoint, 向source 注入 barrier 
+barrier 随数据流流动，算子对齐 barrier 后快照状态， 全节点完成后快照标记为完成。
+两阶段提交
+开启事务数据写入 kafka 但是未提交，等待 checkpoint 完成时提交
+
+key 数据倾斜的问题 C端一般不会，B端会有倾斜
+增加并行度解决数据倾斜问题 
+backpressure 背压 生产速度大于下游消费速度
+
+1 场景 topN
+2 维表关联优化
+
+
+
+加盐方式解决数据倾斜
+
+
+无界 实时流处理 
+有界 离线批处理
+
+微批处理 spark stream 实时计算
+事件驱动 storm flink  真实时计算  时效高 吞吐量低 
+流批一体 etl 
+
+playground 安装flink集群
+
+》 ctl +shift + p 创建工程项目
+flink 1.17.2
+
+
 data-warehouse-learning
 
 解压文件
